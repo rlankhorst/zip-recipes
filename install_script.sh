8677d51c973ab4ca46ec2f0305b48ce6727bc762
Gezim Hoxha <hgezim@gmail.com>
2015-12-13 19:02:39 -0700

    Added comments to install script

--- install_script.sh
+++ install_script.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 sudo apt-get -qq update
 
+echo "Setting up mysql settings."
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -q install -y nginx mysql-server php5-dev php5-mysql php5-fpm php5-xdebug ccze
 
 
+echo "Setting up fpm error logging."
 sudo sh -c "sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php5/fpm/php.ini"
 sudo sh -c "sed -i 's/;php_admin_flag\[log_errors\] = on/;php_admin_flag\[log_errors\] = on/' /etc/php5/fpm/pool.d/www.conf"
 sudo sh -c "sed -i 's!;php_admin_value\[error_log\] = /var/log/fpm-php\.www\.log!php_admin_value\[error_log\] = /var/log/fpm-php\.www\.log!' /etc/php5/fpm/pool.d/www.conf"
 
-# create file for logging
+echo "Creating file for fpm logging."
 sudo touch /var/log/fpm-php.www.log
 sudo chown www-data.www-data /var/log/fpm-php.www.log
 
-# fix xdebug config
+echo "Fixing xdebug settings."
 sudo cat > xdebug_changes << EOF
 xdebug.remote_enable = on
 xdebug.remote_connect_back = on
@@ -23,11 +25,14 @@ EOF
 
 sudo sh -c 'cat xdebug_changes >> /etc/php5/mods-available/xdebug.ini'
 
+echo "Running mysql install script"
 sudo mysql_install_db
 
 
+echo "Downloading WP"
 wget -q http://wordpress.org/latest.tar.gz
 
+echo "Creating wordpress database."
 mysql -u root -proot -e "CREATE DATABASE wordpress; CREATE USER wordpressuser@localhost IDENTIFIED BY 'password';"
 
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost IDENTIFIED BY 'password'; FLUSH PRIVILEGES;"
