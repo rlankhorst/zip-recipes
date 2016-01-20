9df926412f84ac42e9c535656bcdbbead383e559
Gezim Hoxha <hgezim@gmail.com>
2016-01-20 12:36:39 -0700

    Fixed hook call for `zrdn__ratings`

--- class.ziprecipes.php
+++ class.ziprecipes.php
@@ -292,7 +292,7 @@ class ZipRecipes {
 
 		$output .= '<script type="text/javascript" > window.ajaxurl = "' . admin_url('admin-ajax.php') . '";</script>';
 
-		$output .= do_action('zrdn__ratings', $recipe->recipe_id);
+		$output .= apply_filters('zrdn__ratings', '', $recipe->recipe_id);
 
 		// recipe timing
 		if ($recipe->prep_time != null) {
