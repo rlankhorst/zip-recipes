f8c709cd8b5eb0ca25414ca4782a82bc8f5031bc
newash <bpalyi@gmail.com>
2016-03-13 13:01:45 +0000

    add custom Head hook to recipe editor template

--- src/class.ziprecipes.php
+++ src/class.ziprecipes.php
@@ -1048,6 +1048,8 @@ class ZipRecipes {
 		require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 		$settings_page_url = admin_url( 'admin.php?page=' . 'zrdn-settings' );
 
+		$custom_head = apply_filters('ziprecipes_editor_customhead', '');
+
 		Util::print_view('create-update-recipe', array(
 			'pluginurl' => ZRDN_PLUGIN_URL,
 			'recipe_id' => $recipe_id,
@@ -1082,7 +1084,8 @@ class ZipRecipes {
 			'fat' => $fat,
 			'saturated_fat' => $saturated_fat,
 			'notes' => $notes,
-			'submit' => $submit
+			'submit' => $submit,
+			'custom_head' => $custom_head
  		));
 	}
 
