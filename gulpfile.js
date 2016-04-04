b7907b9ae70a2d6497963cb9d55f4952358354ec
Gezim Hoxha <hgezim@gmail.com>
2016-04-04 12:18:24 -0600

    Fixed: when packages are built, vendor only includes non-dev packages
    * Fixed: when packages are built, vendor only includes non-dev packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var zip = require("gulp-zip");
 var shell = require('gulp-shell');
 var sequence = require('run-sequence');
 var path = require("path");
+var fs = require("fs");
+var debug = require("gulp-debug");
 
 const build_path = "build";
 const dest_free = path.join(build_path, "free");
@@ -22,16 +24,6 @@ gulp.task("build-free-js", function () {
     .pipe(gulp.dest(dest_free));
 });
 
-gulp.task("build-premium-vendor", function() {
-  return gulp.src(["src/vendor/twbs/bootstrap/dist/**"], {base: "src"})
-    .pipe(gulp.dest(dest_premium));
-});
-
-gulp.task("build-free-vendor", function() {
-  return gulp.src(["src/vendor/twbs/bootstrap/dist/**"], {base: "src"})
-    .pipe(gulp.dest(dest_free));
-});
-
 /**
  *  Task to build premium version of Zip Recipes.
  */
@@ -42,13 +34,16 @@ gulp.task("build-premium", function () {
   // used to change plugin name to Zip Recipes premium
   var mainPluginFileFilter = filter('zip-recipes.php', {restore: true});
 
-  return gulp.src(["src/**",
+  return gulp.src([
+    "src/**",
     "!src/README.md",
     "!src/composer.*",
     "!node_modules/**",
-    "!src/vendor/**",
+    "!src/vendor-dev/**",
+    "!src/vendor-dev",
     "LICENSE",
-    "!src/plugins/**"])
+    "!src/plugins/**"
+  ])
     // rename premium read me
     .pipe(premiumFileFilter)
     .pipe(rename("README.md"))
@@ -80,13 +75,16 @@ gulp.task("plugins-premium", function () {
 });
 
 gulp.task("build-free", function () {
-  return gulp.src(["src/**",
+  return gulp.src([
+    "src/**",
     "!src/PREMIUM_README.md",
     "!src/composer.*",
     "!node_modules/**",
-    "!src/vendor/**",
+    "!src/vendor-dev/**",
+    "!src/vendor-dev",
     "LICENSE",
-    "!src/plugins/**"])
+    "!src/plugins/**"
+  ])
     // move files to destination
     .pipe(gulp.dest(dest_free));
 });
@@ -98,21 +96,57 @@ gulp.task("compress-free", function() {
 });
 
 /**
- * Run composer command in src dir.
+ * Renames vendor to vendor-dev if if it exists because vendor contains dev packages as well
  */
-gulp.task('composer', shell.task([
-  'cd src && php ../composer.phar install'
+gulp.task('vendor-rename-pre', function(done) {
+  try {
+    return fs.renameSync('src/vendor', 'src/vendor-dev');
+  }
+  catch (e) {
+    console.log("vendor dir not found");
+  }
+  done();
+});
+
+/**
+ * Renames vendor-dev to vendor if if it exists
+ * This restores vendor name for vendor packages that include dev packages
+ */
+gulp.task('vendor-rename-post', function(done) {
+  try {
+    return fs.renameSync('src/vendor-dev', 'src/vendor');
+  }
+  catch (e) {
+    console.log("vendor dir not found");
+  }
+  done();
+});
+
+
+/**
+ * Run composer install --no-dev in src dir.
+ */
+gulp.task('composer-install', shell.task([
+    'cd src && php ../composer.phar install --no-dev'
 ]));
+
+/**
+ * Ensure composer is installed with no dev packages (we don't want to ship dev packages).
+ * To ensure no dev packages are present we rename vendor to vendor-dev then rename it back after the install.
+ */
+gulp.task('composer', function (done) {
+  return sequence('vendor-rename-pre', 'composer-install', 'vendor-rename-post', done);
+});
 
 /**
  * Task to build free version of Zip Recipes.
  */
 gulp.task("free-sequence", function (cb) {
-  return sequence("clean-free", ["composer", "plugins-free", "build-free-js", "build-free", "build-free-vendor"], "compress-free", cb);
+  return sequence("clean-free", ["composer", "plugins-free", "build-free-js"], "build-free", "compress-free", cb);
 });
 
 gulp.task("premium-sequence", function (cb) {
-  return sequence("clean-premium", ["composer", "plugins-premium", "build-premium-js", "build-premium", "build-premium-vendor"], "compress-premium", cb);
+  return sequence("clean-premium", ["composer", "plugins-premium", "build-premium-js"], "build-premium", "compress-premium", cb);
 });
 
 /**
