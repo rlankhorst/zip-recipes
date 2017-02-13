e05bf0fb2348f7eafb5a28ce74943dd7641dff56
Mudassar Ali <sahil_bwp@yahoo.com>
2017-02-13 15:23:49 +0500

    complete work commit

---
+++ src/class.upgrade.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace ZRDN;
+
+/**
+ * Dubugging 
+ */
+if (0) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+/**
+ * Upgrade Base class
+ *
+ * Switch to a real shortcode
+ *
+ * PHP version 5.3
+ *
+ * @package Zip Recipes
+ * @author     Mudassar Ali <sahil_bwp@yahoo.com>
+ * @copyright  2017 Gezim Hoxha
+ */
+class ZipUpgrade extends ZipRecipes {
+
+    /**
+     * Defined Constants
+     */
+    const DESCRIPTION = "Import Recipes from Easy Recipe";
+    const PAGE_ID = "zrdn-upgrade";
+    const MAIN_JS_SCRIPT = "upgrade-js";
+    const MAIN_CSS_SCRIPT = "upgrade-css";
+
+    /**
+     * Initializer for setting up action handler
+     */
+    public static function run() {
+        /**
+         * hook so we can add menus to our admin left-hand menu
+         */
+        add_action('admin_menu', array(get_called_class(), 'admin_menu'));
+
+        /**
+         * Register and enqueue a custom stylesheet in the WordPress admin.
+         */
+        add_action('admin_enqueue_scripts', array(get_called_class(), 'zrdn_enqueue_assets'));
+
+        /**
+         * Notice Hook
+         */
+        add_action('admin_notices', array(get_called_class(), 'zrdn_upgrade_notice_version'));
+
+        /**
+         * Xrh Ajax Call back WP hook
+         */
+        add_action('wp_ajax_upgrade_callback', array(get_called_class(), 'zrdn_ajax_upgrade_callback'));
+    }
+
+    /**
+     * Admin Submenu defined
+     * 
+     * @param type $settings
+     */
+    public static function admin_menu($settings = array()) {
+        $pageTitle = "Upgrade";
+        $menuTitle = $pageTitle;
+        $menuSlug = self::PAGE_ID;
+        $pageRenderer = __CLASS__ . '::zrdn_upgrade_shortcodes';
+        add_submenu_page(
+                'zrdn-settings', $pageTitle, $menuTitle, 'manage_options', $menuSlug, $pageRenderer
+        );
+    }
+
+    /**
+     * Render Import main view
+     */
+    public static function zrdn_upgrade_shortcodes() {
+        Util::print_view("upgrade", array());
+    }
+
+    /**
+     * Show Notice of upgrade
+     */
+    public static function zrdn_upgrade_notice_version() {
+        echo '<div id="message" class="update-nag">'
+        . 'Zip recipes required upgradation. '
+        . '<a href="' . admin_url('admin.php?page=zrdn-upgrade') . '" aria-label="Please update WordPress now">Please update now</a>'
+        . '</div>';
+    }
+
+    /**
+     *  WP registers scripts and styles
+     */
+    public static function zrdn_enqueue_assets() {
+        wp_register_script(self::MAIN_JS_SCRIPT, plugins_url('scripts/upgrade.js', __FILE__), array('jquery'), '1.0', true);
+        wp_enqueue_script(self::MAIN_JS_SCRIPT);
+        wp_register_style(self::MAIN_CSS_SCRIPT, plugins_url('styles/upgrade.css', __FILE__), array(), NULL, 'all');
+        wp_enqueue_style(self::MAIN_CSS_SCRIPT);
+    }
+
+    /**
+     * Implement Xhr callback
+     * 
+     */
+    public static function zrdn_ajax_upgrade_callback() {
+        $request = $_POST;
+        $response = '';
+        $action = $request['zrdn_action'];
+        if (!empty($action)) {
+            switch ($action) {
+                case 'scan':
+                    $response = self::zrdn_scan_deprecated();
+                    break;
+                case 'convert':
+                    $response = self::zrdn_convert_deprecated($request);
+                    break;
+                case 'done':
+                    $response = self::zrdn_convert_deprecated_done();
+                    break;
+                default:
+                    break;
+            }
+        }
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        wp_die();
+        exit();
+    }
+
+    /**
+     * Get All posts and pages
+     * 
+     * @return Array
+     */
+    public static function zrdn_scan_deprecated() {
+        return get_posts(array(
+            'fields' => 'ids', // Only get post IDs
+            'post_type' => array('post', 'page'),
+            'numberposts' => -1,
+            'orderby' => 'ID',
+            'order' => 'ASC',
+        ));
+    }
+
+    /**
+     * 
+     * @param Array $request
+     * @return int
+     */
+    public static function zrdn_convert_deprecated($request) {
+        $found = 0;
+        $id = $request['zrdn_id'];
+        $content = get_post($id);
+        $post_content = ZipUpgrade::zrdn_find_replace($content->post_content);
+        if (strlen($post_content) != strlen($post_content)) {
+            $found = 1;
+        }
+        $content->post_content = $post_content;
+        wp_update_post($content);
+        return $found;
+    }
+
+    /**
+     * Find and replace depricated shortcode
+     * 
+     * @param String $string
+     * @return String
+     */
+    public static function zrdn_find_replace($string) {
+        $string = preg_replace('/\[amd-zlrecipe-recipe:([0-9]+)\]/i', "[ziprecipes recipe id='$1']", $string);
+        $string = preg_replace('/(id)=("(amd-zlrecipe-recipe-)[0-9^"]*")/i', "[ziprecipes recipe id='$1']", $string);
+        return $string;
+    }
+    
+    /**
+     * Update done Option
+     * 
+     * @return type
+     */
+    public static function zrdn_convert_deprecated_done() {
+        return update_option('zrdn_plugin_is_upgrade', 'yes');
+    }
+
+}
+
+ZipUpgrade::run();
+
+
+/**
+ *  end plugin here 
+ */
---
+++ src/scripts/upgrade.js
@@ -0,0 +1,104 @@
+/**
+ * Plugin main js file
+ * 
+ * @param {type} $
+ * @returns {undefined}
+ * php jquery update long pool
+ */
+
+(function ($, window, document, undefined) {
+    var _ids;
+    var initial = 0;
+    var found = 0;
+    var messageAlert = 'Please ensure you have a working backup of your website. This is something you can check with your website hosting company.If you have a working backup of your website, click the checkbox to indicate that.';
+    var scanDeprecated = function () {
+        var queryString = {action: 'upgrade_callback', zrdn_action: 'scan'};
+        $.ajax({
+            type: 'POST',
+            url: ajaxurl,
+            data: queryString,
+            success: function (data) {
+                _ids = data;
+                initial = data.length;
+                return data;
+            }
+        });
+    };
+    scanDeprecated();
+
+    var convertDepr = function () {
+        var deferred = new $.Deferred();
+        var _queue = function (index) {
+            var queryString = {action: 'upgrade_callback', zrdn_action: 'convert', zrdn_id: _ids[index]};
+            $.ajax({
+                type: 'POST',
+                url: ajaxurl,
+                cache: false,
+                async: true,
+                data: queryString,
+                success: function (data) {
+                    deferred.resolve(index);
+                    index++;    // going to next queue entry                    
+                    if (data == 1) {
+                        found++;
+                        var html = (found) + ' found out of ' + initial + '';
+                        $('.progress p:last-child').html(html);
+                    }
+                    // check if it exists
+                    if (_ids[index] != undefined) {
+                        _queue(index);
+                    } else {
+                        if (found == 0) {
+                            $('.progress p:last-child').html('None found out of ' + initial);
+                        }
+                        $(".progress").append("<p>Process finished ...</p>");
+                        $(".steps-success").show();
+                        //_doneDepr();
+                        return deferred.promise();
+                    }
+                }
+            });
+        }; // end of execute_queue() {...
+        if (undefined !== _ids && initial > 0) {
+            var index = 0;
+            _queue(index); // go!            
+        }
+    };
+    
+    var _doneDepr = function () {
+        var queryString = {action: 'upgrade_callback', zrdn_action: 'done'};
+        $.ajax({
+            type: 'POST',
+            url: ajaxurl,
+            data: queryString,
+            success: function (data) {
+                return true;
+            }
+        });
+    };
+
+    var _process = function () {
+        setTimeout(function () {
+            convertDepr();
+        }, 100);
+    };
+
+    $(".upgrade-wrapper section:not(#step-1)").hide();
+    $(document).on("click", "button[name='action_upgrade']", function (e) {
+        e.preventDefault();
+        if (!$('#accept-upgrade').is(":checked")) {
+            if (window.confirm(messageAlert)) {
+                $(".upgrade-wrapper section:nth-child(1)").hide("fast", function () {
+                    $(this).next().show("fast");
+                });
+                _process();
+            }
+        } else {
+            $(".upgrade-wrapper section:nth-child(1)").hide("fast", function () {
+                $(this).next().show("fast");
+            });
+            _process();
+        }
+
+    });
+})(jQuery, window, document);
