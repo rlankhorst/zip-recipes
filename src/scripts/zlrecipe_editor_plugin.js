f855fdb6dc88e4ec17801cee57c74e66842167e2
Gezim Hoxha <hgezim@gmail.com>
2017-05-25 17:37:09 -0600

    Bumped version to 4.13 and updated readme.

--- src/scripts/zlrecipe_editor_plugin.js
+++ src/scripts/zlrecipe_editor_plugin.js
@@ -108,7 +108,7 @@
                 author: 'HappyGezim',
                 authorurl: 'https://www.ziprecipes.net/',
                 infourl: 'https://www.ziprecipes.net/',
-                version: "4.10"
+                version: "4.13"
             };
         }
     });
