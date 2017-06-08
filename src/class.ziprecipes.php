cc7f3d10fff38fea2e01df9c2d73c07b819bb5ea
Gezim Hoxha <hgezim@gmail.com>
2017-06-08 17:46:08 -0600

    Moved clean_jsonld to separate file

--- src/class.ziprecipes.php
+++ src/class.ziprecipes.php
@@ -3,7 +3,9 @@
 namespace ZRDN;
 
 require_once(ZRDN_PLUGIN_DIRECTORY . '_inc/class.ziprecipes.util.php');
+require_once(ZRDN_PLUGIN_DIRECTORY . '_inc/helper_functions.php');
 require_once(ZRDN_PLUGIN_DIRECTORY . '_inc/class.ziprecipes.shortcodes.php');
+
 
 class ZipRecipes {
 
@@ -1561,38 +1563,6 @@ class ZipRecipes {
     }
 
     public static function jsonld($recipe) {
-        /**
-         * Remove blank values from JSON-LD. It looks through nested arrays and considers them to be blank if
-         *  the all of their keys start with @. E.g.: @type, @context.
-         * @param $arr array in JSON LD format.
-         * @return array
-         */
-        function clean_jsonld($arr) {
-            $cleaned_crap = array_reduce(array_keys($arr), function ($acc, $key) use ($arr) {
-                $value = $arr[$key];
-                if (is_array($value)) {
-                    $cleaned_array = clean_jsonld($value);
-                    // add array if it has keys that don't start with @
-                    $array_has_data = count(array_filter(array_keys($cleaned_array), function ($elem) {
-                            return substr($elem, 0, 1) !== '@';
-                        })) > 0;
-
-                    if ($array_has_data) {
-                        $acc[$key] = $cleaned_array;
-                    }
-                }
-                else {
-                    if ($value !== "") {
-                        $acc[$key] = $value;
-                    }
-                }
-
-                return $acc;
-            }, array());
-
-            return $cleaned_crap;
-        }
-
         $formattedIngredientsArray = array();
         foreach (explode("\n", $recipe->ingredients) as $item) {
             $itemArray = self::zrdn_format_item($item);
