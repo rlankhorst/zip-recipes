8930cc0612a9a4e1db619209b58021c10100cfb3
Gezim Hoxha <hgezim@gmail.com>
2017-10-16 13:35:45 -0600

    Updated gulp file for bulma and bulma scss

--- gulpfile.js
+++ gulpfile.js
@@ -303,14 +303,14 @@ gulp.task("premium-sequence-friend", function (cb) {
  * Task to build free and premium versions.
  */
 gulp.task("build", function(cb) {
-  // we need to rename vendor to vendor-dev becaus we don't want to ship vendor dev
+  // we need to rename vendor to vendor-dev because we don't want to ship vendor dev
   return sequence(
     "clean",
     "composer-dev-install",
     // "i18n", // needs vendor to include dev packages
     "vendor-rename-pre",
     "composer-install",
-    ["free-sequence", "premium-sequence-lover", "premium-sequence-admirer", "premium-sequence-friend"],
+    ["sassForMain", "free-sequence", "premium-sequence-lover", "premium-sequence-admirer", "premium-sequence-friend"],
     "vendor-rename-post",
     cb);
 });
@@ -427,3 +427,12 @@ gulp.task('customTemplatesImages', function(cb) {
 gulp.task('custom-templates',
     ['customTemplatesSaas', 'customTemplatesJS', 'customTemplatesFonts', 'customTemplatesImages']
 );
+
+gulp.task('sassForMain', function(cb) {
+    return gulp.src('src/styles/*.scss')
+        .pipe(sass({
+            includePaths: [modules],
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('src/styles/'));
+});
